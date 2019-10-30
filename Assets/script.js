b2911286b1e542188328ac74280f25daf1d932fe
Jean Tubay <jean.tubay.92@gmail.com>
2019-10-30 19:50:23 -0400

    finished adding js for creating display for requested info

--- Assets/script.js
+++ Assets/script.js
@@ -12,12 +12,15 @@ $(document).ready(function() {
         method: "GET"
       }).then(function(response) {
         console.log(response);
-        //$("#temp").text(response.main.temp)
-        // var widget = show(response);
-        // $("#show").html(widget)
+  
         const display = $("#requestedTemp");
+        const newerDiv = $("<div>").css({"border": "solid 4px darkgrey", "margin-top":"10px", "margin-bottom":"10px", "padding-left": "5px"})
         const cityName = $("<h4>").text(response.name + "," +response.sys.country )
-        display.append(cityName)
+        const temp = $("<p>").text("Temperature: " + response.main.temp)
+        const huminity = $("<p>").text("Humidity: " + response.main.humidity)
+        const windSpeed = $("<p>").text("Wind speed: " + response.wind.speed + "mph")
+        newerDiv.append(cityName, temp, huminity, windSpeed)
+        display.prepend(newerDiv)
 
 
 
@@ -29,21 +32,6 @@ $(document).ready(function() {
     }
   });
   
-  // function show(response) {
-  //   return "<h2>Current Weather for "+ response.name+ ", " + response.sys.country + "</h2>" + "<h3><strong>Weather</strong>: "+ response.weather[0].main+"</h3>" + "<h3><strong>Description</strong>: "+ response.weather[0].description+"</h3>"+"<h3><strong>Temperature</strong>: "+response.main.temp+"</h3>"+"<h3><strong>Pressure</strong>: "+response.main.pressure+"</h3>"+"<h3><strong>Humidity</strong>: "+response.main.huminity+"</h3>"+ "<h3><strong>Wind Speed</strong>: " +response.wind.speed+ "</h3>" 
-  // }
-  
-  
-  // $("#secondBtn").on("click", function() {
-  //     var queryURL  = "http://api.openweathermap.org/data/2.5/forecast/daily?q=London&units=metric&cnt=7&appid=166a433c57516f51dfab1f7edaed8413"
-
-  //     $.ajax({
-  //         url: queryURL,
-  //         method: "GET"
-  //     }).then(function(responseTwo){
-  //         console.log(responseTwo)
-  //     })
-  // })
 
 });
 
